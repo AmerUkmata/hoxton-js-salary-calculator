8ead33c82651153b93aedd4228fced338d1ad62e
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-06-13 19:44:13 +0200

    a

---
+++ challenge.js
@@ -0,0 +1,19 @@
+let salery2=200
+let Contribution2=(0.05*salery2)
+console.log(Contribution2)
+console.log(salery2-(Contribution2))
+
+let tax2under80=(0/100)*(80-0)
+console.log(tax2under80)
+
+let tax2under250=(2.4/100)*(250-80)
+console.log(tax2under250)
+
+let tax2under450=(0/100)*(450-250)
+console.log(tax2under450)
+
+let tax2over450=(0/100)*(450+25-450)
+console.log(tax2over450)
+
+let alltax2=(tax2under80+tax2under250+tax2under450+tax2over450)
+console.log(alltax2)
--- index.js
+++ index.js
@@ -12,5 +12,8 @@ console.log(taxunder250)
 let taxunder450=(8/100)*(450-250)
 console.log(taxunder450)
 
-let taxover450=(10/100)*(450-10)
+let taxover450=(10/100)*(450+25-450)
 console.log(taxover450)
+
+let alltax=(taxunder80+taxunder250+taxunder450+taxover450)
+console.log(alltax)
