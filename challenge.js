f59bddb45290f842cc95e445bf326e876e454b47
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-06-13 19:47:56 +0200

    a

--- challenge.js
+++ challenge.js
@@ -6,7 +6,7 @@ console.log(salery2-(Contribution2))
 let tax2under80=(0/100)*(80-0)
 console.log(tax2under80)
 
-let tax2under250=(2.4/100)*(250-80)
+let tax2under250=(4/100)*(250-140)
 console.log(tax2under250)
 
 let tax2under450=(0/100)*(450-250)
