a5c92ecd8d856a2e5f82cf4610ed835609970e90
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-06-13 19:11:51 +0200

    a

---
+++ index.js
@@ -0,0 +1,16 @@
+let salery=500
+let Contribution=(0.05*salery)
+console.log(Contribution)
+console.log(salery-(Contribution))
+
+let taxunder80=(0/100)*(80-0)
+console.log(taxunder80)
+
+let taxunder250=(4/100)*(250-80)
+console.log(taxunder250)
+
+let taxunder450=(8/100)*(450-250)
+console.log(taxunder450)
+
+let taxover450=(10/100)*(450-10)
+console.log(taxover450)
